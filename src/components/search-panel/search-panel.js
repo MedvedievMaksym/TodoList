5f4dbabb1e38a2be35431e5958e090d2dbbb203b
MedvedievM <medvediev.m91@gmail.com>
2019-10-11 21:32:11 +0300

    add search panel

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchPanel = () => {
-	return <input placeholder="search" />;
+export default class SearchPanel extends Component {
+
+    state = {
+        term: ''
+    };
+
+    onSearchChange = (e) => {
+        const term = e.target.value;
+        this.setState({ term });
+        this.props.onSearchChange(term);
+    };
+
+	render() {
+		return (
+			<input
+				type="text" 
+				placeholder="search"
+                className="form-control search-input"
+                value={ this.state.term } 
+                onChange={ this.onSearchChange } />
+		);
+	}
 };
-
-export default SearchPanel;
